26d95611980ab9bb0aee1ff6376b6f4be02a1dee
Dina Saunders <dina_saunders@mac.com>
2020-11-08 17:09:55 -0500

    corrected port typo

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    port: 3006,
+    port: 3306,
     user: 'root',
     password: 'BCSds1826$!',
     database: 'burgers_db'
